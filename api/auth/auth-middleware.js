facb12e3dbb6fcebde41456012910ee3f0df7eb2
nhamilton1 <nickh3592@gmail.com>
2021-10-11 16:01:10 -0400

    Added some middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,6 @@
+const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +9,15 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if(req.session.user) {
+    next()
+  } else {
+    next({
+      message: `invalid credentials`,
+      status: 401
+    })
+  }
 }
 
 /*
@@ -18,8 +28,19 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+async function checkUsernameFree(req, res, next) {
+  try {
+    const { username } = req.body
+    const user = await User.findBy({ username }).first()
+    if (!user) {
+      next()
+    }
+  } catch (err) {
+    next({
+      status: 422,
+      message: "Username taken"
+    })
+  }
 }
 
 /*
@@ -30,8 +51,20 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+async function checkUsernameExists(req, res, next) {
+  try {
+    const { username, password } = req.body
+    const user = await User.findBy({ username }).first()
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user
+      res.status(200).json({ message: `welcome back ${user.username}` })
+    }
+  } catch (err) {
+    next({
+      status: 401,
+      message: "Invalid credentials"
+    })
+  }
 }
 
 /*
@@ -42,8 +75,25 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+async function checkPasswordLength(req, res, next) {
+  try {
+    const { password } = req.body
+    if (password.length < 3) {
+      next()
+    }
+  } catch (err) {
+    next({
+      status: 422,
+      message: "Password must be longer than 3 chars"
+    })
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+}
--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -13,13 +15,38 @@ const cors = require("cors");
 
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
- */
+*/
+
+const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'keep this secret!',
+  cookie: {
+    maxAge: 1000 * 60 * 60, 
+    secure: false, 
+    httpOnly: false,
+  }, 
+  rolling: true, 
+  resave: false, 
+  saveUninitialized: false,
+  store: new Store({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}))
+
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
